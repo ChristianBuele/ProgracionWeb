895cd3474845e52322241f3351b1c204d422ab56
ChristianBuele <christian97.b@hotmail.es>
2020-05-31 01:06:44 -0500

    js login

--- ProyectoWeb/js/login.js
+++ ProyectoWeb/js/login.js
@@ -1,4 +1,5 @@
 const botones = document.getElementById('listaBotones_login');
+const sesion=document.querySelector('#verificacion');
 
 iniciar();
 
@@ -7,5 +8,24 @@ function iniciar(){
 
 }
 function ejecutarBotones(e){
-    console.log(e);
+    e.preventDefault();
+    if (e.target.classList.contains('iniciar')) {
+        leerDatos();
+    }
+    
+
+}
+function leerDatos(e){
+   var usuario = document.getElementById("nomUsuario").value;
+   var contras=document.getElementById("password").value;
+    sesion.innerHTML=``;
+   if(usuario.length==0 | contras.length==0){
+    console.log('ingrese usuario');
+    const validez=document.createElement('h1');
+    validez.innerHTML=`
+    <h1 style="font-size:15px;">Ingrese datos en los campos</h1>
+    `;
+    sesion.appendChild(validez);
+   }
+    
 }
